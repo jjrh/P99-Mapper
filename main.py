9c3c95a0b4f9809d2c070c81acc2e5b29df5312e
Justin Hornosty <justin@credil.org>
2012-03-26 13:26:58 -0400

    more work on map class, has not been tested yet

--- main.py
+++ main.py
@@ -31,10 +31,26 @@ class MAP:
                 self.filename = filename
 
                 self.surface = pygame.Surface((1,1))	# set to 1,1 as default. More there to say this is a surface
+                
+                self.LargestPosX = 0
+                self.LargestPosY = 0
+                self.LargestNegX = 0
+                self.LargestNegY = 0
 
+                self.color = (255,0,0)
         def load(self):
+        """
+        big function, basically it goes:
+        load file.
+        for loop of the lines in the file, for each line we remove the \n, \r tags.
+        we then find out what type it is (L or P)
+        if it's L, we find the largest X and Y, and the smallest X and Y.
+        Create a dictionary of the points then add it to it's respective list.
+        finally we set the class global largestX, largestY, lowestX, lowestY variables. (saves typing self. by doing this once)
+        
+        """
                 try:
-                        f = open(filename)
+                        f = open(self.filename)
                         fail = 0
                         otherFlag = False # when we encounter something unexpected
                         LargestX = 0	# these two are the longest lines, we use this to create a surface that is exactly the right size.
@@ -50,13 +66,14 @@ class MAP:
                                 processedLine = processedLine.split(',')
                                 # we found a line type. 
                                 if('L' in processedLine[0]):
+                                        
                                         fX1 = float(processedLine[1])
                                         fY1 = float(processedLine[2])
                                         fZ1 = float(processedLine[3])
-                                        
                                         fX2 = float(processedLine[4])
                                         fY2 = float(processedLine[5])
                                         fZ2 = float(processedLine[6])
+                                        
 
                                         # These checks have to do with creating a perfect sized surface.
                                         # we need to find the upper X,Y bounds for when we create the surface. 
@@ -88,22 +105,58 @@ class MAP:
                                                 LargestNegY = fY1*-1
                                         if fY2 < 0 and fY2*-1 > LargestNegY:
                                                 LargestNegY = fY1*-1
-                                        
-                                        # Create a dictionary and append it to the L_Points list.
-                                        vectorLine = {'X1': fX1, 'Y1': fY1, 'Z1': fZ1 , 'X2': fX2, 'Y2': fY2, 'Z2': fZ2}
-                                        
-                                        self.L_Points.append(vectorLine)
 
-                                
-                                                                                                                               
-                                                                                                                                                            
-                                                                                                                                                              
-                                                                                                                                                                                            
-                                        
-                                        
-                                
+                                        vectorLine = {'X1': fX1, 'Y1': fY1, 'Z1': fZ1 , 'X2': fX2, 'Y2': fY2, 'Z2': fZ2}
+                                        # Create a dictionary and append it to the L_Points list.
+                                        if(findGrid(fX1,fY1,fX2,fY2) == True):
+                                                self.Grid_Points.append(vectorLine)
+                                        else:                                       
+                                                self.L_Points.append(vectorLine)
+                                                
+                                if ('P' in processedLine[0]):
+                                        self.P_Points.append(processedLine[1:]) # no functionality for these types of points yet.
+
+                        self.LargestPosX = LargestX
+                        self.LargestPosY = LargestY
+                        self.LargestNegX = LargestNegX
+                        self.LargestNegY = LargestNegY
+
                 except:
                         print "Error with load, most likely a bad filename"
+
+        def findGrid(self, x1,y1,x2,y2):
+                # slope = y2-y1 / x2-x1
+                # If slope is undefined (x2-x1 == 0) we have a vertical line. 
+                # if slope is is a whole number then it's a horizontal line. 
+                if(x2-x1) == 0:
+                        print "vertical line"
+                        return True
+                elif ((y2-y1)/(x2-x1))%1 == 0:
+                        # whole number
+                        print "horizontal line"
+                        return True
+                else:
+                        return False
+
+
+        def render(self, ratio=1):
+                if ratio == 0:
+                        print "** error in MAP render(), ratio of 0 would result in dividing by 0. **"
+                        return False
+                else:
+                        self.surface = pygame.Surface((self.LargestPosX+LargestNegX,self.LargestPosY+LargestNegY))
+                        # draw the lines, we add the values their respective largest negative cords, then divide by the ratio. 
+                        for vectorLine in self.L_Points:
+                                x1 = (vectorLine['X1'] + self.LargestNegX)/ratio
+                                x2 = (vectorLine['X2'] + self.LargestNegX)/ratio
+                                
+                                y1 = (vectorLine['Y1'] + self.LargestNegY)/ratio
+                                y2 = (vectorLine['Y2'] + self.LargestNegY)/ratio
+                                pygame.draw.line(self.surface,self.color,(x1,y1),(x2,y2))
+                        return True
+                
+                
+                
         
                 
 
